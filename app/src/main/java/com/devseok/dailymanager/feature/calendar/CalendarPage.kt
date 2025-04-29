da0b623ae1a2ae625862fd89516dc71ba1670eaf
jinseokha <seok270@gmail.com>
2025-04-29 22:29:24 +0900

    [ADD] : Calendar

--- app/src/main/java/com/devseok/dailymanager/feature/calendar/CalendarPage.kt
+++ app/src/main/java/com/devseok/dailymanager/feature/calendar/CalendarPage.kt
@@ -1,13 +1,29 @@
 package com.devseok.dailymanager.feature.calendar
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun CalendarPage(
     navHostController: NavHostController,
     viewModel: CalendarPageBaseVM = hiltViewModel<CalendarPageVM>()
 ) {
 
+    val configuration = LocalConfiguration.current
+
+    BoxWithConstraints(
+        modifier = Modifier
+            .statusBarsPadding()
+    ) {
+
+
+
+    }
 }
--- app/src/main/java/com/devseok/dailymanager/feature/calendar/function/Calendar.kt
+++ app/src/main/java/com/devseok/dailymanager/feature/calendar/function/Calendar.kt
@@ -1,13 +1,37 @@
 package com.devseok.dailymanager.feature.calendar.function
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.devseok.dailymanager.R
+import java.time.LocalDate
 import java.time.YearMonth
 
 @SuppressLint("NewApi")
@@ -41,33 +65,126 @@ fun Calendar(
             .fillMaxSize()
             .background(Color.White)
     ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .align(Alignment.Center)
+                    .padding(start = 8.dp)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = { }
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            block = { append(state.currentPageYM.run { takeIf { year != LocalDate.now().year }?.let { "${year}. $monthValue" } ?: "$monthValue" }) },
+                            style = SpanStyle(
+                                fontSize = 23.sp,
+                                fontWeight = FontWeight.Medium,
+                                baselineShift = BaselineShift(-0.015f)
+                            )
+                        )
+                        state.currentPageYM.takeIf { it.year == LocalDate.now().year }?.let { append("월") }
+                    },
+                    fontSize = 22.sp,
+                    lineHeight = 22.sp,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Image(
+                    painter = painterResource(R.drawable.ic_arrow_down),
+                    contentDescription = "날짜 선택",
+                    modifier = Modifier
+                        .size(22.dp)
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .padding(end = 4.dp)
+            ) {
+                // TODO : 추가 아이콘
 
 
+                // TODO : 알림 아이콘
 
+
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp)
+        ) {
+            listOf("일", "월", "화", "수", "목", "금", "토").forEach { dayText ->
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
+                    Text(
+                        text = dayText,
+                        fontSize = 12.sp,
+                        lineHeight = 17.sp,
+                        fontWeight = FontWeight.Medium,
+                        color = Color.Red.takeIf { dayText == "일" } ?: Color.Black
+                    )
+                }
+            }
+        }
+
+        HorizontalPager(
+            state = state.datePagerState,
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp)
+        ) { page ->
+            val pageYearMonth = remember { YearMonth.from(state.currentDate).plusMonths((page - Int.MAX_VALUE / 2).toLong()) }
+            val dayOfMonth = remember { state.getDaysOfMonth(pageYearMonth) }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                dayOfMonth.chunked(7).forEach { week ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(1f)
+                    ) {
+                        week.forEach { date ->
+                            Box(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                            ) {
+                                val isToday = remember(date, state.currentDate) { date == state.currentDate }
+                                val isSelected = remember(date, state.selectedDate) { date == state.selectedDate }
+                                val isVisibleMonth = remember { YearMonth.from(date) == pageYearMonth }
+
+                                CalendarDay(
+                                    date = date,
+                                    isToday = isToday,
+                                    isSelected = isSelected,
+                                    isVisibleMonth = isVisibleMonth,
+                                    onClick = {
+                                        state.selectedDate = date
+                                        onClick()
+                                    }
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
-
-
-
-
-
-
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
